b0840d8272715e0ebe654f8953e894ccbe567e5e
Anthony Tonatihu Rosales Ceja <arosalesceja1997@gmail.com>
2020-06-01 13:44:39 -0500

    sesion-03 - tabla de multiplicar

--- 01.rb
+++ 01.rb
@@ -1,12 +1,15 @@
-class Holamundo
-  def saluda(x)
-    a = 23
-    puts "Hola mundo "+x+" "+a.to_s
-    puts "Hola #{x} #{a}"
-    # Para convertir numeros a string -> to_s o #{x}
+# frozen_string_literal: true
+
+def tabla(n)
+  i = 0
+  if (n > 0) && (n <= 10)
+    while i <= 10
+      puts "#{n}x#{i} = #{n * i}"
+      i += 1
+    end
   end
 end
 
-obj = Holamundo.new
-obj.saluda("Anthony")
-# gets() con esto se queda parado el proceso
+print 'Ingresa el numero de la tabla: '
+n = gets
+tabla(n.to_i)
